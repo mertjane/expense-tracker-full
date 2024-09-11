af74b39eb9fa84177f1a4022aefeb8367e1b51b6
unknown <mck0391@gmail.com>
2024-09-11 22:04:30 +0100

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+"use client";
 
+import { trpc } from "@/server/client";
 
 export default function Home() {
+  const users = trpc.users.get.useQuery();
+
   return (
-    <div className="border border-red-500">
-      hello expense tracker!
-    </div>
+    <div className="w-full h-screen border border-slate-900 px-4 py-4 text-center">{JSON.stringify(users.data)}</div>
   );
 }
